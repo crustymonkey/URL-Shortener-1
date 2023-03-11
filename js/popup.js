f05f98505161b06c0ecb18aeb3c91899cf045348
Jay Deiman <jay@splitstreams.com>
2023-03-11 10:37:48 -0800

    Started making this better after some basic fixes

--- js/popup.js
+++ js/popup.js
@@ -6,6 +6,14 @@ let longUrlErrorlbl = document.querySelector("#longUrlError");
 let successMsgToast = document.querySelector("#successMsg");
 let errorMsgToast = document.querySelector(".toast-error");
 let loader = document.querySelector('.loading');
+let url_base = "https://t.brk.io";
+
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    // By default, set the url to be shortened to the current tab's url
+    let cur_url = tabs[0].url;
+    console.log(cur_url);
+    $("#longUrl").val(cur_url);
+});
 
 createBtn.addEventListener('click', () => {
     if(longUrlTxt.value){
@@ -15,30 +23,41 @@ createBtn.addEventListener('click', () => {
         errorMsgToast.classList.add("d-hide");
         successMsgToast.classList.add("d-hide");
         loader.classList.remove('d-hide');
+        console.log("in create");
 
         chrome.storage.local.get(['ApiToken'], function (result) {
-        fetch(new URL("https://t.ly/api/v1/link/shorten"), {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({ 
-                "long_url": longUrlTxt.value,
-                "domain": "https://t.ly/",
-                "api_token": result.ApiToken
-            })
-        }).then(response => response.json()).then(json => {
-            shortUrlTxt.classList.remove('d-hide');
-            loader.classList.add('d-hide');                
-            successMsgToast.classList.remove("d-hide");
-            shortUrlTxt.value=json.short_url;
-            longUrlTxt.value="";
-        }).catch(error => {
-            loader.classList.add('d-hide');
-            errorMsgToast.classList.remove("d-hide");
-            errorMsgToast.textContent = error;
-            })
+            $.ajax({
+                type: "POST",
+                url: url_base + "/api/url/create/" + result.ApiToken,
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify({ 
+                    "long_url": longUrlTxt.value,
+                    "short": null
+                }),
+                traditional: true,
+                dataType: "json",
+                success: function(res) {
+                    console.log(res);
+                    if (res.status != "ok") {
+                        loader.classList.add('d-hide');
+                        errorMsgToast.classList.remove("d-hide");
+                        errorMsgToast.textContent = res.message;
+                    }
+                    else {
+                        shortUrlTxt.classList.remove('d-hide');
+                        loader.classList.add('d-hide');                
+                        successMsgToast.classList.remove("d-hide");
+                        shortUrlTxt.value= url_base + "/" + res.short_url;
+                        longUrlTxt.value="";
+                    }
+                },
+                error: function(res) {
+                    console.log(res);
+                    loader.classList.add('d-hide');
+                    errorMsgToast.classList.remove("d-hide");
+                    errorMsgToast.textContent = error;
+                }
+            });
         })
     }
     else{
